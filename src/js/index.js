c025118533afb354a167b18afbbecd09643449f9
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-10-16 12:00:23 +0100

    remove test codes from index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,9 @@
 import '../scss/main.scss';
 import UI from './lib/ui.js';
 import app from './lib/app';
-import project from './lib/factories/project';
-import idGenerator from './lib/util/id_generator';
 
 // UI.initialize();
 
 // Initialize app
 app.initialize();
-
-// Create todo
-// app.createTodo({
-//   title: 'Todo1',
-//   projectId: 11983115232,
-//   dueDate: Date.now(),
-//   priority: 'urgent',
-//   desc: 'Important Todo',
-// });
-
-// Toggle todo status
-
-app.toggleTodoStatus({ projectId: 11983115232, todoId: 597155594894 });
-
 console.log(app.getDB());
